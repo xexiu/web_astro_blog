0ee72ae057a00b89c14f68dc9e7beb01a29ac905
Sergiu Mironescu <smironescu@MacBook-Pro-de-Sergiu.local>
2023-09-21 01:43:44 +0200

    fix typo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
+import mdx from '@astrojs/mdx';
 import partytown from '@astrojs/partytown';
 import sitemap from '@astrojs/sitemap';
 import purgecss from 'astro-purgecss';
 import { defineConfig } from 'astro/config';
 
-import mdx from "@astrojs/mdx";
-
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://candid-malabi-159d6f.netlify.app',
-  integrations: [partytown(), sitemap(), purgecss(), mdx()]
+   site: 'https://candid-malabi-159d6f.netlify.app',
+   integrations: [partytown(), sitemap(), purgecss(), mdx()]
 });
