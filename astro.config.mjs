0c94022fe2f9740a9446dccf6d03d7d921430b10
Sergiu Mironescu <smironescu@MacBook-Pro-de-Sergiu.local>
2023-09-21 00:57:54 +0200

    changes site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-   site: 'http://localhost:4321',
+   site: 'https://candid-malabi-159d6f.netlify.app',
    integrations: [partytown(), sitemap(), mdx(), purgecss()]
 });
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -8,7 +8,7 @@ const posts = formatBlogPosts(Object.values(postImportResult));
 export const GET = () => rss({
    stylesheet: '/rss/styles.xsl',
    title: 'Bienvenid@ a mi blog de desarrollo Full Stack - Sergiu Mironescu',
-   description: 'A humble Astronaut’s guide to the stars',
+   description: 'Explora mi blog para obtener información y consejos sobre desarrollo Full Stack, JavaScript, HTML, CSS, Node.js, Python, React, React Native, Blockchain, WEB3, dApps, smart contracts y más.',
    site: import.meta.env.SITE,
    items: posts.map((post) => ({
       link: post.url,
