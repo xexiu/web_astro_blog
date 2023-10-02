c8f6fe53eb4212d9bec0147b0e6456c5e5f03141
Xexiu <sergiu.mironescu@gmail.com>
2023-10-02 20:59:08 +0200

    bugfix: fixed folders components

--- src/components/postcard/index.ts
+++ src/components/postcard/index.ts
@@ -1,5 +1,11 @@
 export { default as PostCard } from '@components/postcard/PostCard.astro';
 export { default as PostCardCategory } from '@components/postcard/PostCardCategory.astro';
 export { default as PostCardContainer } from '@components/postcard/PostCardContainer.astro';
+export { default as PostCardDetails } from '@components/postcard/PostCardDetails.astro';
+export { default as PostCardDetailsContainer } from '@components/postcard/PostCardDetailsContainer.astro';
+export { default as PostCardExcerpt } from '@components/postcard/PostCardExcerpt.astro';
 export { default as PostCardImage } from '@components/postcard/PostCardImage.astro';
+export { default as PostCardSave } from '@components/postcard/PostCardSave.astro';
+export { default as PostCardTags } from '@components/postcard/PostCardTags.astro';
+export { default as Posts } from '@components/postcard/Posts.astro';
 
