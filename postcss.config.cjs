8ea4359d6a3237f1ab5b26e018f456dcfb469403
Xexiu <sergiu.mironescu@gmail.com>
2023-10-11 19:20:40 +0200

    bugfix: fix build netlify

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,14 +1,14 @@
 module.exports = {
-   plugins: [
-      require('postcss-mixins'),
-      require('postcss-extend'),
-      require('postcss-for'),
-      require('postcss-nesting'),
-      require('postcss-preset-env', {
+   plugins: {
+      'postcss-mixins': {},
+      'postcss-extend': {},
+      'postcss-for': {},
+      'postcss-nesting': {},
+      'postcss-preset-env': {
          'stage': 0
-      }),
-      require('postcss-simple-vars'),
-      require('cssnano'),
-      require('autoprefixer')
-   ]
+      },
+      'postcss-simple-vars': {},
+      'cssnano': {},
+      'autoprefixer': {}
+   }
 };
