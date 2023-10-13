2b6636d9482726ec37774ee0663acdedd8644e7e
Xexiu <sergiu.mironescu@gmail.com>
2023-10-13 08:58:46 +0200

    bugfix: test ssg

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@ import partytown from '@astrojs/partytown';
 import sitemap from '@astrojs/sitemap';
 import purgecss from 'astro-purgecss';
 import { defineConfig, squooshImageService } from 'astro/config';
-import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
-   output: 'server',
    vite: {
       server: {
          watch: {
@@ -18,6 +16,5 @@ export default defineConfig({
    integrations: [partytown(), sitemap(), purgecss()],
    image: {
       service: squooshImageService()
-   },
-   adapter: netlify()
+   }
 });
