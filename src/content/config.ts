a2cf6c4c7ce7abe754af9e8a6b0dd2a971d93545
Xexiu <sergiu.mironescu@gmail.com>
2023-10-17 13:33:53 +0200

    bugfix: fixed config md

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,19 +2,19 @@ import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
    schema: ({ image }) => z.object({
-      title: z.string().max(100),
-      excerpt: z.string().min(90),
+      title: z.string().min(20).max(100),
+      excerpt: z.string().min(50).max(90),
       tags: z.array(z.string()).min(1).max(4),
       category: z.string(),
       is_private: z.boolean().optional(),
       is_portfolio: z.boolean().optional(),
       portfolio_url: z.string().optional(),
-      author: z.string(),
+      author: z.string().min(5).max(50),
       date: z.date(),
       featured_post: z.boolean().optional(),
       featured_image: z.object({
-         src: image().refine((img) => img.width >= 600 && img.height >= 400, {
-            message: 'Cover image must be at least 600 pixels wide and 400 pixels tall!',
+         src: image().refine((img) => img.width >= 200 && img.height >= 200, {
+            message: 'Cover image must be at least 200 pixels wide and 200 pixels tall!',
          }),
          alt: z.string(),
       }),
@@ -23,19 +23,19 @@ const blog = defineCollection({
 
 const portfolio = defineCollection({
    schema: ({ image }) => z.object({
-      title: z.string().max(100),
-      excerpt: z.string().min(90),
+      title: z.string().min(20).max(100),
+      excerpt: z.string().min(50).max(90),
       tags: z.array(z.string()).min(1).max(4),
       category: z.string(),
-      author: z.string(),
+      author: z.string().min(5).max(50),
       date: z.date(),
       github_project_link: z.string().optional(),
       demo_link: z.string().optional(),
       video_link: z.string().optional(),
       featured_portfolio: z.boolean().optional(),
       featured_image: z.object({
-         src: image().refine((img) => img.width >= 600 && img.height >= 400, {
-            message: 'Cover image must be at least 600 pixels wide and 400 pixels tall!',
+         src: image().refine((img) => img.width >= 200 && img.height >= 200, {
+            message: 'Cover image must be at least 200 pixels wide and 200 pixels tall!',
          }),
          alt: z.string(),
       }),
