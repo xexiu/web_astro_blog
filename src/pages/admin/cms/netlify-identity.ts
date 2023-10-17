cc1b5a05e7f8655c58f54a7ee4694ff8d14c4a95
Xexiu <sergiu.mironescu@gmail.com>
2023-10-17 12:41:25 +0200

    bugfix: test

--- src/pages/admin/cms/netlify-identity.ts
+++ src/pages/admin/cms/netlify-identity.ts
@@ -1,14 +1,26 @@
+import { setCookie } from '@js/utils';
+
 export function netlifyIdentityLoad(identity: any) {
    identity.on('init', (user: any) => {
       if (!user) {
          identity.on('login', () => {
+            localStorage.setItem('isAdmin', 'true');
+            setCookie('isAdmin', 'true', 30);
             document.location.href = '/admin/';
          });
       } else {
          identity.on('logout', () => {
+            localStorage.setItem('isAdmin', 'false');
+            setCookie('isAdmin', 'false', 30);
             document.location.href = '/admin/';
          });
       }
+   });
+
+   identity.on('error', (err) => {
+      localStorage.setItem('isAdmin', 'false');
+      setCookie('isAdmin', 'false', 30);
+      console.log('CMS Identity', err);
    });
 
    identity.init();
