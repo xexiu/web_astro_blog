0a51a0cc7aaacbd8e5197a9bdc5107b37d5b5c34
Xexiu <sergiu.mironescu@gmail.com>
2023-10-17 11:24:09 +0200

    bugfix: intent 14

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,13 @@ import { defineMiddleware } from 'astro:middleware';
 
 export const onRequest = defineMiddleware(
    (context, next) => {
-      const isAdmin = context.cookies.get('isAdmin') || '';
-
-      context.cookies.set('isAdmin', isAdmin as any, { domain: '.xexiu.netlify.app' });
+      const isAdmin = context.cookies.get('isAdmin');
 
       if(isAdmin && isAdmin.value) {
+         context.cookies.set('isAdmin', 'true', { domain: '.xexiu.netlify.app' });
+      }
+
+      if(isAdmin) {
          context.cookies.set('isAdmin', 'true', { domain: '.xexiu.netlify.app' });
       }
 
