af3aa8f8953a178076d8821beb1c279106c50e0f
Xexiu <sergiu.mironescu@gmail.com>
2023-10-17 07:19:36 +0200

    bugfix: fix

---
+++ src/middleware.ts
@@ -0,0 +1,15 @@
+import { defineMiddleware } from 'astro/middleware';
+
+export const onRequest = defineMiddleware(
+   (context, next) => {
+      const isAdmin = context.cookies.get('isAdmin');
+
+      if(isAdmin && isAdmin.value) {
+         context.cookies.set('isAdmin', 'true');
+
+      }
+
+      context.cookies.set('hello', 'hello');
+
+      return next();
+   });
