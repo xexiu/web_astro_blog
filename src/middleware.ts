1264456764e068875440543f3d01c6ea5ab630cd
Xexiu <sergiu.mironescu@gmail.com>
2023-10-17 12:01:20 +0200

    bugfix: intent 22

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,15 +4,17 @@ export const onRequest = defineMiddleware(
    (context, next) => {
       const isAdmin = context.cookies.get('isAdmin') || '' as any;
 
-      context.cookies.set('isAdmin', isAdmin, { domain: '.xexiu.netlify.app' });
+      // context.cookies.set('isAdmin', isAdmin, { domain: '.xexiu.netlify.app' });
 
       if(isAdmin && isAdmin.value) {
          context.cookies.set('isAdmin', 'true', { domain: '.xexiu.netlify.app' });
       }
 
       if(isAdmin) {
-         context.cookies.set('isAdmin', 'true', { domain: '.xexiu.netlify.app' });
+         context.cookies.set('isAdmin', 'false', { domain: '.xexiu.netlify.app' });
       }
+
+      context.locals.isAdmin = isAdmin;
 
       return next();
    });
