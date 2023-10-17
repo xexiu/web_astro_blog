960a05e1e1e0aecb53bd1d95f706e5b6002523ab
Xexiu <sergiu.mironescu@gmail.com>
2023-10-17 10:50:02 +0200

    bugfix: intent 4

--- src/netlify-edge-middleware.ts
+++ src/netlify-edge-middleware.ts
@@ -7,6 +7,6 @@ export default function({ request, context }: { request: Request; context: Conte
    return {
       visitorCountry: context.geo.country.name,
       hasEdgeMiddleware: true,
-      isAdmin
+      isAdmin: isAdmin?.value || 'teeest'
    };
 }
