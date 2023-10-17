eae03b891cde18357e1bea73bcbb51a4974fae30
Xexiu <sergiu.mironescu@gmail.com>
2023-10-17 10:22:31 +0200

    bugfix: console.log pwa.js only on dev

--- src/pwa/pwa.ts
+++ src/pwa/pwa.ts
@@ -3,11 +3,15 @@ import { registerSW } from 'virtual:pwa-register';
 registerSW({
    immediate: true,
    onRegisteredSW(swScriptUrl) {
-      // eslint-disable-next-line no-console
-      console.log('SW registered: ', swScriptUrl);
+      if(!import.meta.env.PROD) {
+         // eslint-disable-next-line no-console
+         console.log('SW registered: ', swScriptUrl);
+      }
    },
    onOfflineReady() {
-      // eslint-disable-next-line no-console
-      console.log('PWA application ready to work offline');
+      if(!import.meta.env.PROD) {
+         // eslint-disable-next-line no-console
+         console.log('PWA application ready to work offline');
+      }
    },
 });
