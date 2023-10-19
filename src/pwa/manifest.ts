b56eb2d01a3b4ee4e1d3f72a0d51a6bb981cc69b
Xexiu <sergiu.mironescu@gmail.com>
2023-10-19 07:48:08 +0200

    bugfix: update manifest warnings

--- src/pwa/manifest.ts
+++ src/pwa/manifest.ts
@@ -2,8 +2,39 @@ export const manifest = {
    name: 'Xexiu Dev',
    short_name: 'Xe',
    description: 'Full Stack, JavaScript, HTML, CSS, Node.js, Python, React, React Native, Blockchain, WEB3, dApps, smart contracts y más.',
+   lang: 'es',
    theme_color: '#212129',
    background_color: '#ffffff',
+   categories: [
+      'GENERAL',
+      'CSS',
+      'HTML',
+      'JAVASCRIPT',
+      'FULLSTACK',
+      'FRONTEND',
+      'BACKEND',
+      'REACTJS',
+      'REACT NATIVE',
+      'ASTRO',
+      'BLOCKCHAIN',
+      'MLL',
+      'IA',
+      'WEB3'
+   ],
+   screenshots: [
+      {
+         'src': '/image-default.png',
+         'type': 'image/png',
+         'sizes': '540x720',
+         'form_factor': 'narrow'
+      },
+      {
+         'src': 'image-default.png',
+         'type': 'image/png',
+         'sizes': '1024x593',
+         'form_factor': 'wide'
+      }
+   ],
    icons: [
       {
          src: 'android-chrome-192x192.png',
