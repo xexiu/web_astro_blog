014fe00868742199fc919060082c22cb8788c96f
Xexiu <sergiu.mironescu@gmail.com>
2023-10-20 13:03:59 +0200

    bugfix: test locals and cookies

---
+++ src/middleware.ts
@@ -0,0 +1,19 @@
+import { defineMiddleware } from 'astro:middleware';
+
+export const onRequest = defineMiddleware((context, next) => {
+   const isAdmin = context.cookies.get('isAdmin') || '' as any;
+
+   // context.cookies.set('isAdmin', isAdmin, { domain: '.xexiu.netlify.app' });
+
+   if (isAdmin && isAdmin.value) {
+      context.cookies.set('isAdmin', 'true', { domain: '.xexiu.netlify.app' });
+   }
+
+   if (isAdmin) {
+      context.cookies.set('isAdmin', 'true', { domain: '.xexiu.netlify.app' });
+   }
+
+   context.locals.isAdmin = isAdmin;
+
+   return next();
+});
